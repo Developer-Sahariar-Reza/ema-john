994b71c3be556b07934ca4a5c6bea0f8da8f0782
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-07-08 21:24:04 +0600

    set eventhandler, pass eventhandler to child

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,7 @@ import "./Product.css";
 
 const Product = (props) => {
   const { img, name, seller, quantity, price, ratings, id } = props.product;
+  const handleAddToCart = props.handleAddToCart;
   return (
     <div className="product">
       <img src={img} alt="" />
@@ -12,7 +13,12 @@ const Product = (props) => {
         <p>Manufacturer:{seller}</p>
         <p>Rating: {ratings} Star</p>
       </div>
-      <button className="btn-cart">Add to Cart</button>
+      <button
+        className="btn-cart"
+        onClick={() => handleAddToCart(props.product)}
+      >
+        Add to Cart
+      </button>
     </div>
   );
 };
