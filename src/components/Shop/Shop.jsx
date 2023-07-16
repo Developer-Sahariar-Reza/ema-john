831a35556a5ba3458cb6be365c0f80092471dd42
web-developer-sahariar-reza <web.developer.sahariar.reza@gmail.com>
2023-07-16 21:58:20 +0600

    Load cart from local storage, find product

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -11,9 +12,18 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      const quantity = storedCart[id];
+      addedProduct.quantity = quantity;
+    }
+  }, [products]);
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
   return (
     <div className="shop-container">
