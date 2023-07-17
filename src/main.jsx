2e0bfce605e230c71c7d67c58dfc56bb897ed041
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-07-17 08:58:11 +0600

    react router setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,34 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Shop from "./components/Shop/Shop.jsx";
+import Home from "./components/Layout/Home.jsx";
+import Orders from "./components/Orders/Orders.jsx";
+import Inventory from "./components/Inventory/Inventory.jsx";
+import Login from "./components/Login/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Home />,
+    children: [
+      {
+        path: "/",
+        element: <Shop />,
+      },
+      {
+        path: "orders",
+        element: <Orders />,
+      },
+      {
+        path: "inventory",
+        element: <Inventory />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
