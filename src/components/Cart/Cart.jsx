e995e2827f9692b17f9ad5f1044b32b8c02202c2
Developer-Sahariar-Reza <dev.sahariar.reza@gmail.com>
2023-07-17 08:29:33 +0600

    Handle quantity from storage to cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,18 @@ import "./Cart.css";
 const Cart = ({ cart }) => {
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping;
+    totalPrice = totalPrice + product.price * product.quantity;
+    totalShipping = totalShipping + product.shipping * product.quantity;
+    quantity = quantity + product.quantity;
   }
   const tax = (totalPrice * 10) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
-      <p>Selected Items: {cart.length}</p>
+      <p>Selected Items: {quantity}</p>
       <p>Total Price: ${totalPrice}</p>
       <p>Shipping Charge: ${totalShipping}</p>
       <p>Tax: ${tax.toFixed(2)}</p>
